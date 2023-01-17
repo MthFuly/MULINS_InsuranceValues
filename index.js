114025956fb7bcdc73c5838f8d919ba2081027e9
MthFuly <59844142+MthFuly@users.noreply.github.com>
2023-01-17 10:03:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const res = require('express/lib/response');
+const req = require('express/lib/request');
+const { response } = require('express');
+const port = process.env.PORT || 21263
+
+// Configurando o servidor
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors());
+
+let db = {
+    "returnCod":"0", 
+    "message":""
+  }
+
+  let dberror = {
+    "returnCod":"4", 
+    "message":""
+  }
+
+  // Buscar dados
+  app.get('/users/insuranceValues', (req, res) =>{
+    return res.json(db);
+  });
+
+  // Inserir dados
+  app.post('/users/insuranceValues', (req, res) =>{
+      const body = req.body;
+    if(!body){
+         return res.json(dberror);
+    }
+    else{
+        return res.json(db);
+    }
+  });
+
+app.listen(port, () =>{
+    console.log('Servidor está rodando ');
+})
+// app.listen(21263, () => {
+//     console.log('Express started at http://localhost:21263');
+// })
+
+
